c254bddb0306b5f18a90973ed049f98e97bae9bc
Ghulam Qadir <24bsai31@quest.edu.pk>
2026-06-01 21:40:18 +0500

    Add Introduction.py with a simple Python program

---
+++ Introduction.py
@@ -0,0 +1,13 @@
+# Your first Python program
+print("Hello, World!")
+
+# Using comments
+# This is a single-line comment
+"""
+This is a 
+multi-line comment (docstring)
+"""
+
+# Getting input
+name = input("Enter your name: ")
+print(f"Welcome, {name}!")
