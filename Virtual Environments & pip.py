a507d47ca608db5a446b9dcdbf6edc59584128a6
Ghulam Qadir <24bsai31@quest.edu.pk>
2026-06-25 15:05:27 +0500

    Add virtual environment setup instructions
    Added instructions for creating and managing a Python virtual environment using pip.

---
+++ Virtual Environments & pip.py
@@ -0,0 +1,27 @@
+# This is conceptual - run these in terminal
+
+"""
+# Create virtual environment
+python -m venv myenv
+
+# Activate (Windows)
+myenv\Scripts\activate
+
+# Activate (Mac/Linux)
+source myenv/bin/activate
+
+# Install packages
+pip install requests numpy pandas
+
+# List installed
+pip list
+
+# Save requirements
+pip freeze > requirements.txt
+
+# Install from requirements
+pip install -r requirements.txt
+
+# Deactivate
+deactivate
+"""
